2e2fd31334b9210ed8e6f8e9c68d042fa915fc29
Jeevesh8 <48825663+Jeevesh8@users.noreply.github.com>
2020-03-11 14:42:55 +0530

    Update CNN_LSTM.py

--- Models/CNN_LSTM.py
+++ Models/CNN_LSTM.py
@@ -4,13 +4,12 @@ import torch
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class cnn_lstm(nn.Module) :
-    def __init__(self,seq_len=128, ini_len=18, final_len=1, batch_size=256) :
+    def __init__(self,seq_len=128, ini_len=18, final_len=1) :
         super().__init__()
         self.d_model = 20 
         self.seq_len = seq_len
         self.hidden_size = 32
         self.num_layers = 1
-        self.batch_size=batch_size
         self.init_trnsfrm = nn.Sequential(nn.Linear(ini_len,32),nn.ReLU(),nn.Linear(32,32),nn.ReLU(),nn.Linear(32,self.d_model))
         self.batch_norm = nn.BatchNorm1d(self.d_model)
         self.cnn = nn.Sequential(nn.Conv1d(self.d_model, 32,4,1),
@@ -22,9 +21,8 @@ class cnn_lstm(nn.Module) :
         self.final = nn.Sequential(nn.Linear(self.hidden_size*56,512),nn.ReLU(),nn.Linear(512,final_len))
     
     def forward(self,batch) :
-        h_n, c_n = torch.ones((self.num_layers,self.batch_size,self.hidden_size),dtype=torch.float64, device=device),torch.ones((self.num_layers,self.batch_size,self.hidden_size),dtype=torch.float64, device=device)
         batch = self.cnn(self.batch_norm(self.init_trnsfrm(batch).transpose(1,2))).transpose(1,2)
-        batch,(h_n,c_n) = self.lstm( batch , (h_n,c_n))
+        batch,(h_n,c_n) = self.lstm(batch)
         del h_n,c_n
         out = self.final(batch.reshape(-1,self.hidden_size*56))
         return out
