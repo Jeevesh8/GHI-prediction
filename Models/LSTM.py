fd85125e1cc03837fc3ed5310c703ffd5cdee08f
Jeevesh8 <48825663+Jeevesh8@users.noreply.github.com>
2020-03-13 15:06:37 +0530

    Create LSTM.py

---
+++ Models/LSTM.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch
+
+class cnn_lstm(nn.Module) :
+    def __init__(self, seq_len=seq_len, ini_len=18, final_len=1) :
+        super().__init__()
+        self.d_model = ini_len 
+        self.seq_len = seq_len
+        self.hidden_size = 32
+        self.num_layers = 1
+        self.lstm = nn.LSTM(self.d_model,self.hidden_size,self.num_layers,batch_first=True)
+        self.final = nn.Sequential(nn.Linear(self.hidden_size*self.seq_len,512),nn.ReLU(),nn.Linear(512,final_len))
+        
+    def forward(self,batch) :
+        batch, (_b,_a) = self.lstm( batch )
+        del _b, _a
+        out = self.final(batch.reshape(-1,self.hidden_size*self.seq_len))
+        return out
